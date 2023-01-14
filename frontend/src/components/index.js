f91e581435c1bc9eef19948ff7e42ae2994c66df
Minod <minod.perera1721@gmail.com>
2023-01-14 16:48:49 +0530

    new changes - tailwind model

---
+++ frontend/src/components/index.js
@@ -0,0 +1,27 @@
+import Billing from "./Billing";
+import Business from "./Business";
+import CardDeal from "./CardDeal";
+import Client from "./Client";
+import CTA from "./CTA";
+import FeedbackCard from "./FeedbackCard";
+import Footer from "./Footer";
+import GetStarted from "./GetStarted";
+import Hero from "./Hero";
+import Navbar from "./Navbar";
+import Testimonials from "./Testimonials";
+import Stats from "./Stats";
+
+export{
+    Navbar,
+    Billing,
+    Business,
+    Client,
+    CTA,
+    FeedbackCard,
+    Footer,
+    GetStarted,
+    Hero,
+    Testimonials,
+    CardDeal,
+    Stats
+}
